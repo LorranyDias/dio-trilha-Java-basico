976eeffb880093f5b00e77d81891a44330a0103c
Lorrany-Dias <docemelhany18@gmail.com>
2024-05-29 16:27:24 -0300

    tipos-variaveis

---
+++ tipo-variaveis/src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+         //tipos primitivos 
+         //estudem a class string que representa texto na aplicação
+
+         String  meuNome = "LORRANY DA SILVA DIAS"
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+		System.out.print(numero);
+
+       final double VALOR_DE_PI = 3.14;
+
+        
+
+    }
+}
